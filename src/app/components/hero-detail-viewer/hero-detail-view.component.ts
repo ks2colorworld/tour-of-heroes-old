49853877596dd6b6d3e819d6f0a01237979b84ba
kslee <ks@colorworld.kr>
2020-06-02 22:48:53 +0900

    [#23] 소스코드 구조 개선

--- src/app/components/hero-detail-viewer/hero-detail-view.component.ts
+++ src/app/components/hero-detail-viewer/hero-detail-view.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Hero } from '../hero';
+import { Hero } from 'src/app/classes/hero';
 
 @Component({
   selector: 'app-hero-detail-view',
