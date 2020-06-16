09cd75205953124114e155f9efe493971f0f76e8
kslee <ks@colorworld.kr>
2020-06-16 10:28:31 +0900

    회원가입 component 등록

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  registerForm: FormGroup;
+  errorMessage = '';
+  successMessage = '';
+
+  constructor(
+    public authService: AuthService,
+    private router: Router,
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm(): void {
+    this.registerForm = this.fb.group({
+      email: ['', Validators.required ],
+      password: ['', Validators.required]
+    });
+  }
+
+  tryFacebookLogin(): void {
+    this.authService.doFacebookLogin()
+    .then(res => {
+      this.router.navigate(['/user']);
+    }, err => console.log(err)
+    );
+  }
+
+  tryTwitterLogin(): void {
+    this.authService.doTwitterLogin()
+    .then(res => {
+      this.router.navigate(['/user']);
+    }, err => console.log(err)
+    );
+  }
+
+  tryGoogleLogin(): void {
+    this.authService.doGoogleLogin()
+    .then(res => {
+      this.router.navigate(['/user']);
+    }, err => console.log(err)
+    );
+  }
+
+  tryRegister(value: { email: string; password: string; }): void {
+    this.authService.doRegister(value)
+    .then(res => {
+      console.log(res);
+      this.errorMessage = '';
+      this.successMessage = 'Your account has been created';
+    }, err => {
+      console.log(err);
+      this.errorMessage = err.message;
+      this.successMessage = '';
+    });
+  }
+
+}
