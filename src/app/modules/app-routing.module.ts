e8d437cdd26a26428b7e080c8ce172e9feee1b09
kslee <ks@colorworld.kr>
2020-06-16 10:30:10 +0900

    등록된 component routing.module에 등록

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+import { AuthGuard } from '../core/auth.guard';
+import { UserResolver } from '../core/user.resolver';
+
 import { DashboardComponent } from '../components/dashboard/dashboard.component';
 import { HeroDetailComponent } from '../components/hero-detail/hero-detail.component';
 import { HeroesComponent } from '../components/hero-list/heroes.component';
 import { AttachmentListContainerComponent } from '../components/attachment-list-container/attachment-list-container.component';
 import { SampleNavComponent } from '../sample/__sample-nav/sample-nav.component';
+import { LoginComponent } from '../components/login/login.component';
+import { RegisterComponent } from '../components/register/register.component';
+import { UserComponent } from '../components/user/user.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
@@ -13,6 +20,9 @@ const routes: Routes = [
   { path: 'heroes', component: HeroesComponent },
   { path: 'attachment', component: AttachmentListContainerComponent },
   { path: 'sample', component: SampleNavComponent },
+  { path: 'login', component: LoginComponent, canActivate: [AuthGuard] },
+  { path: 'register', component: RegisterComponent, canActivate: [AuthGuard] },
+  { path: 'user', component: UserComponent,  resolve: { data: UserResolver}}
 ];
 
 
