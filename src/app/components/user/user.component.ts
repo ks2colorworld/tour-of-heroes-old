0076e9f70b301294c0f6d515414d45dba2b18ed9
kslee <ks@colorworld.kr>
2020-06-16 23:47:45 +0900

    Update user.component

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+
+import { FirebaseUserModel } from 'src/app/core/user.model';
+import { UserService } from 'src/app/core/user.service';
+import { AuthService } from 'src/app/core/auth.service';
+import { UtilService } from 'src/app/services/util.service';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +15,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  user: FirebaseUserModel = new FirebaseUserModel();
+  profileForm: FormGroup;
+
+  constructor(
+    public userService: UserService,
+    public authService: AuthService,
+    public util: UtilService,
+    private route: ActivatedRoute,
+    private location: Location,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.route.data.subscribe(routeData => {
+      const data = routeData.data;
+      if (data) {
+        this.user = data;
+        this.createForm(this.user.name);
+      }
+    });
   }
 
+  createForm(name: string) {
+    this.profileForm = this.fb.group({
+      name: [name, Validators.required ]
+    });
+  }
+
+  save(value: { name: string; }){
+    this.userService.updateCurrentUser(value)
+    .then(res => {
+      console.log(res);
+    }, err => console.log(err));
+  }
+
+  logout(){
+    this.authService.doLogout()
+    .then((res) => {
+      this.location.back();
+    }, (error) => {
+      console.log('Logout error', error);
+    });
+  }
 }
